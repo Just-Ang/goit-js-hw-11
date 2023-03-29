8109f7bdecd02a9ed01c2b4ee83544122f6bbadd
Angelica <lisoveckaya1997@gmail.com>
2023-03-29 21:05:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// const axios = require('axios/dist/browser/axios.cjs');
+const axios = require('axios/dist/browser/axios.cjs');
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
@@ -11,33 +11,36 @@ let pages = 1;
 
 formEl.addEventListener('submit', onSubmitClick);
 let inputValue = '';
-function fetchImg(name, pagee) {
-  return fetch(
-    `https://pixabay.com/api/?key=34821995-346cc43bb02fb642b37e66530&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pagee}`
-  ).then(response => {
-
-    return response.json();
-  });
-
+async function fetchImg(name, pagee) {
+  try {
+    const response = await fetch(
+      `https://pixabay.com/api/?key=34821995-346cc43bb02fb642b37e66530&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pagee}`
+    );
+    const pictures = await response.json();
+    return pictures;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 function onSubmitClick(e) {
   e.preventDefault();
   inputValue = inputEl.value.trim();
-let page = 1;
+  let page = 1;
   fetchImg(inputValue, page).then(data => {
     console.log(data);
 
     if (data.total === 0) {
-         return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-         
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
     }
 
-   if (data.total >= 1) {
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-    const items = data.hits
-    .map(
-      item => `<a class="gallery__item" href="${item.largeImageURL}" onclick="event.preventDefault()">
+    if (data.total >= 1) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      const items = data.hits
+        .map(
+          item => `<a class="gallery__item" href="${item.largeImageURL}" onclick="event.preventDefault()">
       <div class="photo-card"> 
       <img src="${item.webformatURL}" alt="${item.tags}" title="${item.tags}" class="photo" loading="lazy" />
       <div class="info">
@@ -56,34 +59,31 @@ let page = 1;
       </div> </div>
 
   </a>`
-    )
-    .join('');
-    moreBtn.style.opacity = "1";
+        )
+        .join('');
+      moreBtn.style.opacity = '1';
 
-  galleryEl.innerHTML = items;
-  var lightbox = new SimpleLightbox('.gallery a', {
-      captionSelector: 'img',
-      captionsData: 'alt',
-      captionPosition: 'bottom',
-      captionDelay: 250,
-      scrollZoom: false,
-    });
-   }
-      
+      galleryEl.innerHTML = items;
+      var lightbox = new SimpleLightbox('.gallery a', {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        scrollZoom: false,
+      });
+    }
   });
-  
 }
 
-
-moreBtn.addEventListener('click', onBtnClick)
-
+moreBtn.addEventListener('click', onBtnClick);
 
 function onBtnClick() {
-    pages += 1;
-    
-    fetchImg(inputValue, pages).then(data => {
-        console.log(data);
-        const items = data.hits
+  pages += 1;
+
+  fetchImg(inputValue, pages).then(data => {
+    console.log(data);
+
+    const items = data.hits
       .map(
         item => `<a class="gallery__item" href="${item.largeImageURL}" onclick="event.preventDefault()">
         <div class="photo-card"> 
@@ -107,16 +107,13 @@ function onBtnClick() {
       )
       .join('');
 
-    galleryEl.insertAdjacentHTML("beforeend", items);
+    galleryEl.insertAdjacentHTML('beforeend', items);
     var lightbox = new SimpleLightbox('.gallery a', {
-        captionSelector: 'img',
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-        scrollZoom: false,
-      });
-    
-    })
-
-
+      captionSelector: 'img',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+      scrollZoom: false,
+    });
+  });
 }

b2dc2c283cd8562eee205568175f10b20751c09f
Angelica <lisoveckaya1997@gmail.com>
2023-03-29 15:39:26 +0300

    simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,55 +1,60 @@
-// const axios = require('axios/dist/browser/axios.cjs'); 
+// const axios = require('axios/dist/browser/axios.cjs');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
-const galleryEl = document.querySelector('.gallery')
+const galleryEl = document.querySelector('.gallery');
 
 formEl.addEventListener('submit', onSubmitClick);
 let inputValue = '';
 function fetchImg(name) {
-    return fetch(
-        `https://pixabay.com/api/?key=34821995-346cc43bb02fb642b37e66530&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-      ).then(response => {
-        return response.json();
-      });
+  return fetch(
+    `https://pixabay.com/api/?key=34821995-346cc43bb02fb642b37e66530&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
+    return response.json();
+  });
 }
 
 function onSubmitClick(e) {
-    e.preventDefault()
-    inputValue = inputEl.value.trim();
+  e.preventDefault();
+  inputValue = inputEl.value.trim();
 
-fetchImg(inputValue).then(data => {
-    console.log(data)
-    let html = '';
-    for (const img of data.hits) {
-        const webformatURL = img.webformatURL;
-        const largeImageURL = img.largeImageURL;
-        const tags = img.tags;
-        const likes = img.likes;
-        const views = img.views;
-        const comments = img.comments;
-        const downloads = img.downloads;
+  fetchImg(inputValue).then(data => {
+    console.log(data);
 
-        html += `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" class="photo" loading="lazy" />
+    const items = data.hits
+      .map(
+        item => `<a class="gallery__item" href="${item.largeImageURL}" onclick="event.preventDefault()">
+        <div class="photo-card"> 
+        <img src="${item.webformatURL}" alt="${item.tags}" title="${item.tags}" class="photo" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>likes: ${likes}s</b>
+            <b>likes: ${item.likes}s</b>
           </p>
           <p class="info-item">
-            <b>views: ${views}</b>
+            <b >views: ${item.views}</b>
           </p>
           <p class="info-item">
-            <b>comments: ${comments}</b>
+            <b>comments: ${item.comments}</b>
           </p>
           <p class="info-item">
-            <b> downloads: ${downloads}</b>
+            <b > downloads: ${item.downloads}</b>
           </p>
-        </div>
-      </div>`
-       galleryEl.innerHTML = html;
-      }
-    
-})
+        </div> </div>
+ 
+    </a>`
+      )
+      .join('');
 
+    galleryEl.innerHTML = items;
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        scrollZoom: false,
+      });
+    
+  });
 }
